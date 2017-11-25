ed9919915bc99a71850e33c4a1ae99a0b7c016a8
Tuomo Hopia <tuomo.hopia@gmail.com>
2017-11-25 09:28:26 +0200

    Moved init items to middleware

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import flyVibeApp from './reducers'
 
-const url =
-  'https://api.instagram.com/oauth/authorize/?client_id=925edd4e1ada41f0b0e00ab519691d73&redirect_uri=http://localhost:3000&response_type=token&scope=likes+comments+public_content';
+let store = createStore(flyVibeApp)
 
-const token = localStorage.getItem('insta_token');
-
-if (token) {
-  fetch(
-    `https://api.instagram.com/v1/users/self/media/liked?access_token=${token}`
-  )
-    .then(res => res.json())
-    .then(console.log);
-
-  ReactDOM.render(<App />, document.getElementById('root'));
-  registerServiceWorker();
-} else {
-  const [param, token] = window.location.hash.split('=');
-
-  if (param === '#access_token') {
-    localStorage.setItem('insta_token', token);
-  } else {
-    window.location.replace(url);
-  }
-}
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
