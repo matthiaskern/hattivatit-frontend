4eb158c9b330787061a2d8ceb45d5c1604d7c9f7
Matthias Kern <matthias_kern@posteo.net>
2017-11-25 21:38:40 +0200

    use instagram images

--- src/components/InstagramReelContainer.js
+++ src/components/InstagramReelContainer.js
@@ -1,14 +1,8 @@
 import { connect } from 'react-redux'
 import InstagramReel from './InstagramReel'
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
     return {
-      sources: [
-          'https://www.interrail.eu/sites/interrail.eu/files/assets/images/2017/01/italy_pass_regular_desktop.jpg',
-          'https://www.eurail.com/sites/eurail.com/files/styles/asset_image_images_slider_big/public/assets/images/2017/02/eiffel_tower_paris_france_.jpg?itok=z0o9aKKb',
-          'http://static5.businessinsider.com/image/56410ed2bd86ef135c8bd715/32-things-everyone-should-do-in-germany.jpg',
-          'http://www.telegraph.co.uk/content/dam/video_previews/x/5/x5cgi0ode66q6vuxezqmehmexwer6bt-xlarge.jpg'
-        ]
     }
   }
 
