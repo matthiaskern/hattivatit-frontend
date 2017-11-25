853df66a72cab7da1d524841e6d3087b88198462
Matthias Kern <matthias_kern@posteo.net>
2017-11-25 20:13:38 +0200

    give instagram api url to getoffers api

--- src/middleware/initMiddleware.js
+++ src/middleware/initMiddleware.js
@@ -1,85 +1,104 @@
 import registerServiceWorker from '../registerServiceWorker';
-import { FETCH_LIKES, RECEIVE_OFFERS, FETCH_OFFERS, INIT, RECEIVE_LIKES, SET_TOKEN } from '../actions';
+import {
+  FETCH_LIKES,
+  RECEIVE_OFFERS,
+  FETCH_OFFERS,
+  INIT,
+  RECEIVE_LIKES,
+  SET_TOKEN
+} from '../actions';
 
 const CLIENT_ID = '925edd4e1ada41f0b0e00ab519691d73';
 
+const unique = array => [...new Set(array)];
 
-const unique = array => [...new Set(array)]
+export default function instagramMiddleware() {
+  // Use ES6 functional currying
+  return store => {
+    registerServiceWorker();
 
-export  default function instagramMiddleware() {
-    // Use ES6 functional currying
-    return store => {
+    function init() {
+      const url = `https://api.instagram.com/oauth/authorize/?client_id=${
+        CLIENT_ID
+      }&redirect_uri=${
+        window.location.origin
+      }&response_type=token&scope=likes+comments+public_content`;
 
-        registerServiceWorker()
+      const [param, token] = window.location.hash.split('=');
 
-        function init() {
-            const url = `https://api.instagram.com/oauth/authorize/?client_id=${CLIENT_ID}&redirect_uri=${window.location.origin}&response_type=token&scope=likes+comments+public_content`;
-
-                const [param, token] = window.location.hash.split('=');
-    
-                if (param === '#access_token') {
-                    store.dispatch({ type: SET_TOKEN, payload: token })
-                } else {
-
-                    setTimeout( () => { 
-                        window.location.replace(url);
-                    }, 500);
-                }
-        }
-
-        function fetchLikes() {
-          
-          const token = store.getState().instaToken;
-          if (token) {
-
-            const url = `https://api.instagram.com/v1/users/self/media/liked?access_token=${token}`;
-            fetch(url).then(res => res.json()).then(data => store.dispatch({type: RECEIVE_LIKES, payload: data}))
-          }
-
-        }
-        
-        const offersUrl = 'https://hattivatit.azurewebsites.net/api/getofferlist';
-
-        function fetchOffers(locations = ['Berlin']) {
-          const headers = new Headers();
-          headers.append("Content-Type", "application/json");
-
-          const commonPayload = {
-            from: 'Helsinki', 
-            dateFrom: '2018-01-01',
-            dateTo: '2018-02-08'
-          };
-
-          const uniqueLocations = unique(locations )
-          const payloads = uniqueLocations.filter(name => !name.toLowerCase().startsWith('rey')).map(to => ({...commonPayload, to}));
-
-
-
-          return fetch(offersUrl, { method: 'POST', headers, body: JSON.stringify(payloads)}).then(res => res.json()).then(data => store.dispatch({type: RECEIVE_OFFERS, payload: data}))
-        }
-
-        return next => action => {
-            console.log(action)
-
-            switch (action.type) {
-                case INIT:
-                  init();
-                  break;
-        
-                case FETCH_LIKES:
-                  fetchLikes();
-                  break;
-
-                case FETCH_OFFERS:
-                  fetchOffers(action.payload);
-                  break;
-
-                default:
-
-                    break
-            }
-
-            return next(action)
-        }
+      if (param === '#access_token') {
+        store.dispatch({ type: SET_TOKEN, payload: token });
+      } else {
+        setTimeout(() => {
+          window.location.replace(url);
+        }, 500);
+      }
     }
+
+    function fetchLikes() {
+      const token = store.getState().instaToken;
+      if (token) {
+        const url = `https://api.instagram.com/v1/users/self/media/liked?access_token=${
+          token
+        }`;
+        fetch(url)
+          .then(res => res.json())
+          .then(data => store.dispatch({ type: RECEIVE_LIKES, payload: data }));
+      }
+    }
+
+    const offersUrl = 'https://hattivatit.azurewebsites.net/api/getofferlist';
+
+    function fetchOffers(locations = ['Berlin']) {
+      const headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+
+      const commonPayload = {
+        from: 'Helsinki',
+        dateFrom: '2018-01-01',
+        dateTo: '2018-02-08'
+      };
+      const { instaToken } = store.getState();
+
+      const uniqueLocations = unique(locations);
+      const payloads = uniqueLocations
+        .filter(name => !name.startsWith('rey'))
+        .map(to => ({
+          ...commonPayload,
+          to,
+          instaUrl: `https://api.instagram.com/v1/tags/${to}/media/recent?access_token=${instaToken}`
+        }));
+
+      return fetch(offersUrl, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(payloads)
+      })
+        .then(res => res.json())
+        .then(data => store.dispatch({ type: RECEIVE_OFFERS, payload: data }));
+    }
+
+    return next => action => {
+      console.log(action);
+
+      switch (action.type) {
+        case INIT:
+          init();
+          break;
+
+        case FETCH_LIKES:
+          fetchLikes();
+          break;
+
+        case FETCH_OFFERS:
+          fetchOffers(action.payload);
+          break;
+
+        default:
+          break;
+      }
+
+      return next(action);
+    };
+  };
 }
